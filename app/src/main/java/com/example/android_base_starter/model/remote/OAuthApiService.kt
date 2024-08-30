2e0583f0cd690571d1e27db0dea812634ef95753
michaelSyntax <michael.karbe@syntax-institut.de>
2024-08-30 10:55:23 +0200

    Prepare for OAuth

---
+++ app/src/main/java/com/example/android_base_starter/model/remote/OAuthApiService.kt
@@ -0,0 +1,43 @@
+package com.example.android_base_starter.model.remote
+
+import com.example.android_base_starter.BuildConfig
+import com.example.android_base_starter.model.data.googleApi.Location
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OAuthApiService {
+    @GET("maps/api/place/textsearch/json")
+    suspend fun getPlacesForTextSearch(
+        @Query("key") apiKey: String = BuildConfig.apiKey,
+        @Query("query") query: String): Location
+
+    @GET("maps/api/place/nearbysearch/json")
+    suspend fun getPlacesNearby(
+        @Query("location") location: String,
+        @Query("radius") radius: String,
+        @Query("type") type: String,
+        @Query("key") apiKey: String = BuildConfig.apiKey): Location
+}
+
+object OAuthApi {
+    private const val BASE_URL = "https://maps.googleapis.com/"
+    private val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    private val client = OkHttpClient.Builder().addInterceptor(logger).build()
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    private val converter = MoshiConverterFactory.create(moshi)
+
+    val service: OAuthApiService by lazy {
+        Retrofit.Builder()
+            .addConverterFactory(converter)
+            .baseUrl(BASE_URL)
+            .client(client)
+            .build()
+            .create(OAuthApiService::class.java)
+    }
+}
