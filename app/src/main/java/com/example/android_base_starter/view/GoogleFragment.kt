4d622e29635c6dc2c4008ec0c5ec98dd7223bef8
michaelSyntax <michael.karbe@syntax-institut.de>
2024-08-30 03:12:28 +0200

    Add BottomNavigationView; Add menu Add new Fragments

---
+++ app/src/main/java/com/example/android_base_starter/view/GoogleFragment.kt
@@ -0,0 +1,36 @@
+package com.example.android_base_starter.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.android_base_starter.R
+import com.example.android_base_starter.databinding.FragmentGoogleBinding
+import com.example.android_base_starter.databinding.FragmentMockBinding
+import com.example.android_base_starter.view.adapter.ItemAdapter
+import com.example.android_base_starter.viewModel.MainViewModel
+
+class GoogleFragment : Fragment() {
+    private lateinit var vb: FragmentGoogleBinding
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        vb = FragmentGoogleBinding.inflate(layoutInflater)
+        viewModel.getItems()
+        return vb.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.items.observe(viewLifecycleOwner) { items ->
+
+        }
+    }
+}
